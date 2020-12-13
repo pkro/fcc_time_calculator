3b649e830d69de1a3b6f5a20a1e39b5ffc788f6e
pkro <pkrombach@web.de>
2020-12-13 09:43:07 +0100

    initial

---
+++ time_calculator.py
@@ -0,0 +1,9 @@
+def add_time(start, duration, weekday):
+  start_time = (start[:-3], start[-3:])
+  if start_time[1] == " AM":
+    start_time[0] = int(start_time[0][:start_time[0].index(':')]
+
+
+
+
+    return new_time
